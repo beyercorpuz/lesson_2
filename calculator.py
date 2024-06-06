1b4a8305df5404bf66b94d23481d3ea64635c579
beyercorpuz <beyercorpuz@gmail.com>
2024-06-06 11:31:20 -0400

    Update calcularor program

--- calculator.py
+++ calculator.py
@@ -1,16 +1,38 @@
 def prompt(message):
     print(f'==> {message}')
 
-prompt('Welcome to Calculator!')
 
-prompt("what's the first number?")
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+
+prompt('Welcome to Calculator')
+
+prompt("What's the first number?")
 number1 = input()
-prompt("what's the second number?")
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
 number2 = input()
 
-prompt('what operation would you like to perform?\n'
-      '1) Add 2) Subtact 3) Multiply 4) Divide')
+while invalid_number(number2):
+    prompt("Hmm...that doesn't look like a valid number.")
+    number2 = input()
+
+prompt('What operation would you like to perform?\n) 1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
+
+while operation not in ['1','2','3','4']:
+    prompt('You must choose 1.2.3, or 4')
+
 
 match operation:
     case '1':
@@ -23,5 +45,11 @@ match operation:
         output = int(number1) / int(number2)
 
 print(f'The result is {output}')
-  
-        
+
+
+
+
+
+
+
+
